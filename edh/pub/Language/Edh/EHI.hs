e27f5ab659106c26c1b87442f7242acc37f6964a
Compl Yue <compl.yue@icloud.com>
2020-03-16 00:57:11 +0800

    ehi update

--- edh/pub/Language/Edh/EHI.hs
+++ edh/pub/Language/Edh/EHI.hs
@@ -92,6 +92,7 @@ module Language.Edh.EHI
   , Expr(..)
   , Prefix(..)
   , Literal(..)
+  , AttrName
   , AttrAddr(..)
   , AttrAddressor(..)
   , ArgsReceiver(..)
@@ -138,6 +139,7 @@ module Language.Edh.EHI
   , nil
   , edhNone
   , edhNothing
+  , noneNil
   , true
   , false
   , nan
@@ -155,6 +157,10 @@ module Language.Edh.EHI
   , mkHostClass
   , mkHostOper
 
+    -- * args pack parsing
+  , ArgsPackParser(..)
+  , parseArgsPack
+
     -- * Monadic API for textual interpreter
   , runEdh
   , runEdhWithoutBatteries
@@ -171,6 +177,7 @@ import           Control.Exception
 import           Control.Monad.Reader
 
 import           Data.Text                     as T
+import qualified Data.HashMap.Strict           as Map
 
 import           Text.Megaparsec
 
@@ -216,4 +223,28 @@ runEdhWithoutBatteries !logger (ReaderT f) =
 type EdhShell a = ReaderT (EdhWorld, Object) IO a
 
 type EdhBootstrap a = ReaderT EdhWorld IO a
+
+
+data ArgsPackParser a = ArgsPackParser {
+    pos'parsers :: [EdhValue -> a -> Either Text a]
+    , kw'parsers :: Map.HashMap AttrName (EdhValue ->  a -> Either Text a)
+  }
+parseArgsPack :: a -> ArgsPackParser a -> ArgsPack -> Either Text a
+parseArgsPack defVal (ArgsPackParser posParsers kwParsers) (ArgsPack posArgs kwArgs)
+  = go posParsers kwParsers posArgs (Map.toList kwArgs) defVal
+ where
+  go
+    :: [EdhValue -> a -> Either Text a]
+    -> Map.HashMap AttrName (EdhValue -> a -> Either Text a)
+    -> [EdhValue]
+    -> [(AttrName, EdhValue)]
+    -> a
+    -> Either Text a
+  go _  _    []      []                     result = Right result
+  go [] _    (_ : _) _                      _      = Left "too many args"
+  go _  kwps []      ((kwn, kwv) : kwargs') result = case Map.lookup kwn kwps of
+    Nothing  -> Left $ "unknown arg: " <> kwn
+    Just kwp -> kwp kwv result >>= go [] kwps [] kwargs'
+  go (p : posParsers') kwps (arg : args') kwargs result =
+    p arg result >>= go posParsers' kwps args' kwargs
 
