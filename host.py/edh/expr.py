258ff93ac24b43bbab4d196e31f96659d42a07b2
ComplYue <compl.yue@icloud.com>
2020-10-15 17:23:53 +0800

    expr fixup

--- host.py/edh/expr.py
+++ host.py/edh/expr.py
@@ -1,7 +1,9 @@
 """
-An expr() function at par to expr construct in Edh
+The expr class at par to expr construct in Edh
 
-Unlike in Edh, expr values in Python stay of string type
+The interpolation happens just once at time of construction of an expr object,
+an expr object responds to `repr()` without quoted, otherwise it is not
+different to the vanilla string of the interpolated result.
 
 """
 __all__ = ["expr"]
@@ -16,20 +18,32 @@ import inspect
 SPLITER = re.compile(r"\{\$(.*?)\$\}", re.S)
 
 
-def expr(isrc: str) -> str:
+class expr:
     """
-    Generate source with possible interpolations from caller's context
-
+    Generate source of an expression with possible interpolations of values
+    from caller's context
     """
-    caller_frame = inspect.currentframe().f_back
-    globals_ = caller_frame.f_globals
-    locals_ = caller_frame.f_locals
 
-    segs_out, segs_in = [], SPLITER.split(isrc)
-    while True:
-        lit_seg, *segs_in = segs_in
-        segs_out.append(lit_seg)
-        if not segs_in:
-            return "".join(segs_out)
-        intpl_seg, *segs_in = segs_in
-        segs_out.append(repr(eval(intpl_seg, globals_, locals_)))
+    __slots__ = ("interpolated",)
+
+    def __init__(self, isrc: str):
+        caller_frame = inspect.currentframe().f_back
+        globals_ = caller_frame.f_globals
+        locals_ = caller_frame.f_locals
+
+        segs_out, segs_in = [], SPLITER.split(isrc)
+        while True:
+            lit_seg, *segs_in = segs_in
+            segs_out.append(lit_seg)
+            if not segs_in:
+                self.interpolated = "".join(segs_out)
+                break
+            intpl_seg, *segs_in = segs_in
+            segs_out.append(repr(eval(intpl_seg, globals_, locals_)))
+
+    def __str__(self):
+        return self.interpolated
+
+    # don't get quoted when converting to repr string
+    __repr__ = __str__
+
