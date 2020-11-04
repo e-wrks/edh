781572c6e8cafa89425c9a0c79743f4cda472cef
ComplYue <compl.yue@icloud.com>
2020-11-04 19:29:59 +0800

    fixup: evt virtual attrs

--- host.hs/pub/Language/Edh/Batteries/Evt.hs
+++ host.hs/pub/Language/Edh/Batteries/Evt.hs
@@ -6,7 +6,10 @@ import           Prelude
 
 import           Control.Concurrent.STM
 
+import           Language.Edh.Args
+import           Language.Edh.Control
 import           Language.Edh.Details.RtTypes
+import           Language.Edh.Details.Evaluate
 
 
 -- | virtual property <sink>.mrv
@@ -16,19 +19,26 @@ import           Language.Edh.Details.RtTypes
 -- this can't tell a sink's state as marked end-of-stream by a nil data,
 -- or no event has ever been posted into it, in both cases this will
 -- return nil
-sink'mrvProc :: EventSink -> EdhHostProc
-sink'mrvProc !sink !exit !ets =
-  readTVar (evs'mrv sink) >>= \ !mrv -> exitEdh ets exit mrv
+sink'mrvProc :: "sinkValue" !: EdhValue -> EdhHostProc
+sink'mrvProc (mandatoryArg -> !sinkVal) !exit !ets =
+  case edhUltimate sinkVal of
+    EdhSink !sink -> readTVar (evs'mrv sink) >>= \ !mrv -> exitEdh ets exit mrv
+    _             -> edhValueDesc ets sinkVal $ \ !badDesc ->
+      throwEdh ets UsageError $ "not an event sink but a " <> badDesc
 
 
 -- | virtual property <sink>.eos
 --
 -- check whether an event sink is already at end-of-stream, which is marked
 -- by a nil data
-sink'eosProc :: EventSink -> EdhHostProc
-sink'eosProc !sink !exit !ets = readTVar (evs'seqn sink) >>= \case
-  0 -> exitEdh ets exit $ EdhBool False
-  _ -> readTVar (evs'mrv sink) >>= \case
-    EdhNil -> exitEdh ets exit $ EdhBool True
-    _      -> exitEdh ets exit $ EdhBool False
+sink'eosProc :: "sinkValue" !: EdhValue -> EdhHostProc
+sink'eosProc (mandatoryArg -> !sinkVal) !exit !ets =
+  case edhUltimate sinkVal of
+    EdhSink !sink -> readTVar (evs'seqn sink) >>= \case
+      0 -> exitEdh ets exit $ EdhBool False
+      _ -> readTVar (evs'mrv sink) >>= \case
+        EdhNil -> exitEdh ets exit $ EdhBool True
+        _      -> exitEdh ets exit $ EdhBool False
+    _ -> edhValueDesc ets sinkVal $ \ !badDesc ->
+      throwEdh ets UsageError $ "not an event sink but a " <> badDesc
 
