f09f8795100cbf53623d8802d9bf7c9a081d2e79
ComplYue <compl.yue@icloud.com>
2020-11-07 03:03:06 +0800

    add edhm executable to run module without repl assumption

--- host.hs/runmodu/Main.hs
+++ host.hs/runmodu/Main.hs
@@ -17,21 +17,43 @@ import qualified Data.Text                     as T
 
 import           Language.Edh.EHI
 
-import           Repl
-
 
 main :: IO ()
 main = getArgs >>= \case
-  []        -> runModu "repl"
-  [edhModu] -> runModu edhModu
-  _         -> hPutStrLn stderr "Usage: edh [ <edh-module> ]" >> exitFailure
- where
-  runModu :: FilePath -> IO ()
-  runModu !moduSpec = do
-    !console <- defaultEdhConsole defaultEdhConsoleSettings
-    let !consoleOut = writeTBQueue (consoleIO console) . ConsoleOut
+  [moduSpec] -> do
 
-    void $ forkFinally (edhProgLoop moduSpec console) $ \ !result -> do
+    !console <- defaultEdhConsole defaultEdhConsoleSettings
+    let !consoleOut      = writeTBQueue (consoleIO console) . ConsoleOut
+        !consoleShutdown = writeTBQueue (consoleIO console) ConsoleShutdown
+        consoleErr msg =
+          consoleLogger console 50 Nothing $ ArgsPack [EdhString msg] odEmpty
+        runIt = do
+
+          world <- createEdhWorld console
+          installEdhBatteries world
+          runEdhModule world moduSpec edhModuleAsIs >>= \case
+            Left !err -> atomically $ do
+              -- program crash on error
+              consoleErr
+                $  T.pack
+                $  "Edh crashed with an error:\n"
+                <> show err
+                <> "\n"
+              consoleShutdown
+            Right !phv -> case edhUltimate phv of
+              -- clean program halt, all done
+              EdhNil -> return ()
+              -- unclean program exit
+              _      -> atomically $ do
+                consoleErr
+                  $  (<> "\n")
+                  $  "Edh halted with a result:\n"
+                  <> case phv of
+                       EdhString msg -> msg
+                       _             -> T.pack $ show phv
+                consoleShutdown
+
+    void $ forkFinally runIt $ \ !result -> do
       case result of
         Left (e :: SomeException) ->
           atomically $ consoleOut $ "💥 " <> T.pack (show e)
@@ -39,10 +61,6 @@ main = getArgs >>= \case
       -- shutdown console IO anyway
       atomically $ writeTBQueue (consoleIO console) ConsoleShutdown
 
-    atomically $ do
-      consoleOut ">> Bare Đ (Edh) Interpreter <<\n"
-      consoleOut
-        "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
-      consoleOut "  https://github.com/e-wrks/edh/tree/master/Tour\n"
-
     consoleIOLoop console
+
+  _ -> hPutStrLn stderr "Usage: edhm <edh-module>" >> exitFailure
