aed8c6e0df3baa51cba6f0d3d82a338a7cc95e41
ComplYue <compl.yue@icloud.com>
2021-06-17 18:57:04 +0800

    fmt upd

--- host.hs/repl/Main.hs
+++ host.hs/repl/Main.hs
@@ -17,14 +17,13 @@ main =
     _ -> hPutStrLn stderr "Usage: edh [ <edh-module> ]" >> exitFailure
   where
     replWithModule :: FilePath -> IO ()
-    replWithModule = edhRepl defaultEdhConsoleSettings $
-      \ !world -> do
-        let !consoleOut = consoleIO (edh'world'console world) . ConsoleOut
+    replWithModule = edhRepl defaultEdhConsoleSettings $ \ !world -> do
+      let !consoleOut = consoleIO (edh'world'console world) . ConsoleOut
 
-        -- install all necessary batteries
-        installEdhBatteries world
+      -- install all necessary batteries
+      installEdhBatteries world
 
-        consoleOut $
-          ">> Bare Đ (Edh) Interpreter <<\n"
-            <> "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
-            <> "  https://github.com/e-wrks/tour\n"
+      consoleOut $
+        ">> Bare Đ (Edh) Interpreter <<\n"
+          <> "* Blank Screen Syndrome ? Take the Tour as your companion, checkout:\n"
+          <> "  https://github.com/e-wrks/tour\n"
