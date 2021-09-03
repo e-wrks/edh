2d3746153077443be761341650524ed7a890e979
ComplYue <compl.yue@icloud.com>
2021-09-03 17:51:59 +0800

    expose err api for MHI

--- host.hs/src/Language/Edh/MHI.hs
+++ host.hs/src/Language/Edh/MHI.hs
@@ -10,6 +10,14 @@ module Language.Edh.MHI
     module Language.Edh.InterOpM,
     module Language.Edh.Batteries.Args,
     module Language.Edh.Batteries.Comput,
+
+    -- ** Edh Runtime error
+    getEdhErrCtx,
+    edhCreateError,
+    throwHostSTM,
+    throwHostSTM',
+    throwHostIO,
+    throwHostIO',
   )
 where
 
@@ -18,6 +26,7 @@ import Language.Edh.Args
 import Language.Edh.Batteries.Args
 import Language.Edh.Batteries.Comput
 import Language.Edh.Control
+import Language.Edh.Evaluate
 import Language.Edh.HostEvs
 import Language.Edh.IOPD
 import Language.Edh.InterOpM
