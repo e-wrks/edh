03549400af76254eca3238717f39d9c9d5422251
ComplYue <compl.yue@icloud.com>
2022-01-20 22:00:24 +0800

    wip

--- host.hs/src/Language/Edh/Batteries/Evt.hs
+++ host.hs/src/Language/Edh/Batteries/Evt.hs
@@ -2,10 +2,13 @@ module Language.Edh.Batteries.Evt where
 
 -- import           Debug.Trace
 
--- import Language.Edh.Evaluate
+import Control.Concurrent.STM
+import qualified Data.HashMap.Strict as Map
+-- import Language.Edh.IOPD
+import Language.Edh.Control
+import Language.Edh.Monad
 import Language.Edh.RtTypes
-
--- import Prelude
+import Prelude
 
 -- | operator (:-) - event deriving rule definition
 evtDerivOp :: EdhIntrinsicOp
@@ -16,3 +19,56 @@ evtDerivOp _lhExpr _rhExpr !exit =
 evtActOp :: EdhIntrinsicOp
 evtActOp _lhExpr _rhExpr !exit =
   exitEdhTx exit nil
+
+data EvtInterest = EvtInterest
+  { evt'stream :: !Sink,
+    evt'capture'whole :: !(Maybe Symbol),
+    evt'capture'fields :: ![(AttrKey, Symbol)]
+  }
+
+data EvtStep = EvtStep
+  { evt'concurrent :: ![EvtInterest],
+    evt'step'guard :: !(Maybe (Edh Bool))
+  }
+
+data EvtRule = EvtRule
+  { evt'match'seq :: ![EvtStep],
+    evt'incidents ::
+      !( TVar
+           ( Map.HashMap EvtFabrics EvtIncident,
+             Map.HashMap Sink [EvtIncident]
+           )
+       )
+  }
+
+-- | Common attribute keys & values the fabrication of one specific rule match
+-- must hold.
+--
+-- Only symbols appeared in more than one upstream event capture are considered
+-- common attribute.
+newtype EvtFabrics = EvtFabrics (Map.HashMap Symbol EdhValue)
+
+-- | All attribute keys & values one specific rule match is evaluated against.
+--
+-- Including all symbols appeared in any upstream event capture.
+newtype EvtContext = EvtContext (Map.HashMap Symbol EdhValue)
+
+-- | In-progress (or including finished?) fabrication (matching) of a rule.
+--
+-- Incidents with same 'EvtFabrics' are mutually exclusive.
+data EvtIncident = EvtIncident
+  { evt'curr'match :: !EvtFabrics,
+    evt'curr'step :: !EvtStep,
+    evt'missing :: ![EvtInterest],
+    evt'subseqs :: ![EvtStep],
+    evt'context :: !EvtContext
+  }
+
+perceiveEvt :: EvtRule -> Edh () -> Sink -> Object -> Edh ()
+perceiveEvt (EvtRule [] _) _ _ _ = throwEdhM EvalError "empty event rule"
+perceiveEvt (EvtRule mseq@(step1 : subseqs) !inciVar) !act !evs !evo = do
+  return ()
+  where
+    matchEvt :: EvtIncident -> Edh (Maybe EvtIncident)
+    matchEvt !inci =
+      return Nothing
